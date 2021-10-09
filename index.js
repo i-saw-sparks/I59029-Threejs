16926abc4cfd3563a1f02e0407473b85de92cf01
ok <ok>
2021-10-09 12:31:14 -0500

    new material

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 var geometry = new THREE.OctahedronGeometry(1,1)
-var material = new THREE.MeshBasicMaterial({ color: 0xff0051 })
+var material = new THREE.MeshBasicMaterial({  color: 0xff0051, flatShading: true, metalness: 0, roughness: 1})
 var cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 renderer.render(scene, camera)
 camera.position.z = 5
-
 
 var ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
@@ -18,8 +17,6 @@ scene.add(ambientLight)
 var pointLight = new THREE.PointLight( 0xffffff, 1 );
 pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
-
-var material = new THREE.MeshStandardMaterial( { color: 0xff0051 })
 
 
 function animate() {
