fcca423941fd51eaa5cd0e83ce8dc957760ee5a4
ok <ok>
2021-10-09 12:17:48 -0500

    cube

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer({ antialias: true})
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1)
+var material = new THREE.MeshBasicMaterial( { color: 0xff0051 })
+var cube = new THREE.Mesh ( geometry, material )
+scene.add( cube )
+renderer.render( scene, camera )
+camera.position.z = 5
